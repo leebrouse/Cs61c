2a826206fa92c2f42c2ed811343fb735f3de83b7
root <root@LAPTOP-LDK0D967>
2024-05-24 22:20:27 +0800

    complete 2 exercises

--- fa20-lab-starter/lab02/bit_ops.c
+++ fa20-lab-starter/lab02/bit_ops.c
@@ -3,25 +3,49 @@
 
 // Return the nth bit of x.
 // Assume 0 <= n <= 31
-unsigned get_bit(unsigned x,
-                 unsigned n) {
+unsigned get_bit(unsigned x,unsigned n) {
     // YOUR CODE HERE
     // Returning -1 is a placeholder (it makes
     // no sense, because get_bit only returns 
     // 0 or 1)
-    return -1;
+    if (n>=0&&n<=31)
+    {
+       unsigned mask=1<<n;
+       unsigned bit=(x&mask)>>n; 
+       return bit;
+    }else{
+        return -1;
+    }
+    
+    
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
-void set_bit(unsigned * x,
-             unsigned n,
-             unsigned v) {
+void set_bit(unsigned * x,unsigned n,unsigned v) {
     // YOUR CODE HERE
+    if (n>=0&&n<=31&&(v==0||v==1))
+    {
+       unsigned mask=1<<n;
+       if (v==0)
+       {
+        *x&=~mask;
+       }else{
+        *x|=mask;
+       }
+       
+    }
+    
+   
+    
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
-void flip_bit(unsigned * x,
-              unsigned n) {
+void flip_bit(unsigned * x,unsigned n) {
     // YOUR CODE HERE
+    if (n>=0&&n<=31)
+    {
+       unsigned mask=1<<n;
+       *x^=mask; 
+    }
 }
 
--- fa20-lab-starter/lab02/lfsr.c
+++ fa20-lab-starter/lab02/lfsr.c
@@ -6,5 +6,12 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t lfsr=*reg; 
+    uint16_t feedback;           
+
+    feedback = ((lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5)) & 1u;
+    lfsr = (lfsr >> 1) | (feedback << 15);
+
+    *reg=lfsr;   
 }
 
